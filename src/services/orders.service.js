6f7fc2823abe6647ae783ae9f1c56da8ec1029d3
arshik18 <arshik18@gmail.com>
2020-09-02 15:39:18 +0530

    tool tip added

--- src/services/orders.service.js
+++ src/services/orders.service.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import * as endpoints from '../services/endpoints';
 
 export const postOrdersService = (data) => {
-  //console.log(data,"data");
     return axios.post(endpoints.orders,data);
   };
+  
 export const getOrdersService = () => {
     return axios.get(endpoints.orders);
   };
